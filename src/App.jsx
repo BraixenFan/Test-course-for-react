5c61981d45751b962399c66a94fd8ede1574113e
Carlos Armando Plata Hernández <carlos.plata@gmail.com>
2023-03-21 00:03:05 -0600

    Tackled React router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+        <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
